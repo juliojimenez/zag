0789f8f57e11cf132f662ccb8fa9f314f274931a
Julio Jimenez <julio@julioj.com>
2024-02-25 22:27:12 -0500

    while loops
    Signed-off-by: Julio Jimenez <julio@julioj.com>

---
+++ while-loops/src/main.zig
@@ -0,0 +1,40 @@
+const expect = @import("std").testing.expect;
+const std = @import("std");
+
+test "while" {
+    var i: u8 = 2;
+    while (i < 100) {
+        i *= 2;
+    }
+    try expect(i == 128);
+}
+
+test "while with continue expression" {
+    var sum: u8 = 0;
+    var i: u8 = 1;
+    while (i <= 10) : (i += 1) {
+        sum += 1;
+    }
+    try expect(sum == 10);
+}
+
+test "while with continue" {
+    var sum: u8 = 0;
+    var i: u8 = 0;
+    while (i <= 3) : (i += 1) {
+        if (i == 2) continue;
+        sum += i;
+    }
+    try expect(sum == 4);
+}
+
+test "while with break" {
+    var sum: u8 = 0;
+    var i: u8 = 0;
+    while (i <= 3) : (i += 1) {
+        std.debug.print("{d}\n", .{sum});
+        if (i == 2) break;
+        sum += 1;
+    }
+    try expect(sum == 2);
+}
